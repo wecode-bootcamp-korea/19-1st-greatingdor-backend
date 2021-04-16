42b72faccc371a466e86fedeefb0dc324d99cc2e
GyeongJae <gyeongjae.h.dev@gmail.com>
2021-04-16 17:41:43 +0900

    Add: 상품 출력 기능 구현 - 메인페이지 ALL(전체상품) 출력 기능 - category_id filter 기능 - 메인페이지 카테고리 호버 창 메뉴 이름, 카테고리 이름 출력 기능

--- greatingdor/urls.py
+++ greatingdor/urls.py
@@ -13,7 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path
+from django.urls import path, include
+from products.views import CategoryView
 
 urlpatterns = [
+    path('products', include('products.urls')),
+    path('categories', CategoryView.as_view()),
 ]
--- products/views.py
+++ products/views.py
@@ -1,3 +1,55 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.http  import JsonResponse
+from django.views import View
+
+from .models      import ProductImage, Product, Menu, Tag, ProductTag
+
+class MainView(View):
+    def get(self, request):
+        category_id = request.GET.get('category', None)
+
+        if category_id != None:
+            infos  = Product.objects.filter(category_id=category_id)
+        else:    
+            infos  = Product.objects.all()
+
+        page = int(request.GET.get("page", 1))
+        page_size = 6
+        limit = int(page_size * page)
+        offset = int(limit - page_size)
+        
+        result = []
+        for info in infos:
+            images  = info.productimage_set.all()
+            image_list = []
+            for image in images:
+                image_list.append(image.image_url)
+            result.append(
+            {
+            'image': image_list,
+            'price': info.price,
+            'title': info.title,
+            'description': info.description,
+            'category': info.category.name,
+            'isnew': True
+            }
+            )
+        return JsonResponse({'RESULT':result[offset:limit]}, status=200)
+
+class CategoryView(View):
+    def get(self, request):
+        menus = Menu.objects.all()
+        result = []
+        for menu in menus:
+            categories = menu.category_set.all()
+            category_list = []
+            for category in categories:
+                category_list.append(category.name)
+            result.append(
+            {
+            'menu': menu.name,
+            'category': category_list,
+            }
+            )
+        return JsonResponse({'RESULT':result}, status=200)
