16691e85538f1ba34d151dd4272a795529016d8c
GyeongJae <gyeongjae.h.dev@gmail.com>
2021-04-20 14:26:16 +0900

    Add: 검색 기능 추가 - products/views.py SearchView 작성 - products/urls.py 경로 설정

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import MainView, CategoryView
+from .views import MainView, CategoryView, SearchView
 
 urlpatterns = [
     path('', MainView.as_view()),
     path('/categories', CategoryView.as_view()),
+    path('/search', SearchView.as_view()),
 ]
