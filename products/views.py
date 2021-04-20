44b0d6a7105ba5827d16c956ecd4b586037fa7a5
GyeongJae <gyeongjae.h.dev@gmail.com>
2021-04-20 12:26:12 +0900

    Refactor: products/views.py 코드수정 - 중복되는 코드 부분 수정 - code align 부분 수정

--- products/views.py
+++ products/views.py
@@ -1,11 +1,11 @@
 import json
 import random
 
-from django.http  import JsonResponse
-from django.views import View
+from django.http      import JsonResponse
+from django.views     import View
 from django.db.models import Q
 
-from .models      import ProductImage, Product, Menu, Tag, ProductTag, Category
+from .models          import ProductImage, Product, Menu, Tag, ProductTag, Category
 
 class MainView(View):
     def get(self, request):
@@ -13,34 +13,32 @@ class MainView(View):
 
         if category_id != None:
             products = Product.objects.filter(category_id=category_id)
-            infos = products.order_by('description')
         else:    
             products = Product.objects.all()
-            infos = products.order_by('description')
-        
+            
+        infos  = products.order_by('description')
         result = [{
-            'image': [image.image_url for image in info.productimage_set.all()],
-            'price': info.price,
-            'title': info.title,
+            'image'      : [image.image_url for image in info.productimage_set.all()],
+            'price'      : info.price,
+            'title'      : info.title,
             'description': info.description,
-            'category': info.category.name,
-            'isnew': info.productdetail_set.all().first().is_new,
-            'store': info.productdetail_set.all().first().store_method,}
+            'category'   : info.category.name,
+            'isnew'      : info.productdetail_set.all().first().is_new,
+            'store'      : info.productdetail_set.all().first().store_method,}
             for info in infos]
             
-        page = int(request.GET.get("page", 1))
+        page      = int(request.GET.get("page", 1))
         page_size = 6
-        limit = int(page_size * page)
-        offset = int(limit - page_size)
+        limit     = int(page_size * page)
+        offset    = int(limit - page_size)
             
         return JsonResponse({'RESULT':result[offset:limit]}, status=200)
 
 class CategoryView(View):
     def get(self, request):
-        menus = Menu.objects.all()
         result = [{
-            'menu': menu.name,
-            'category': [category.name for category in menu.category_set.all()],}
-            for menu in menus]
+            'menu'     : menu.name,
+            'category' : [category.name for category in menu.category_set.all()],}
+            for menu in Menu.objects.all()]
         return JsonResponse({'RESULT':result}, status=200)
     
