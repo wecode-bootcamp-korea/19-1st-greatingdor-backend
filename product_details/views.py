76536bbfd8ef53f03fd469596b09de4f42e76c95
Chan <prestoferoce@gmail.com>
2021-04-22 12:25:18 +0900

    Add: product review view - product review view 생성 - 반복적으로 product 확인 작업 따로 함수 구분 - 오류 메시지 상수 생성

--- product_details/views.py
+++ product_details/views.py
@@ -3,61 +3,72 @@ import json
 from django.views import View
 from django.http  import JsonResponse
 
-from products.models import Product
+from products.models        import Product
+from product_details.models import ProductReview
 
+MESSAGE = {
+    "404": "NOT_FOUND",
+}
 
 class ProductDetailView(View):
     def get(self, request, product_id):
-        try:
-            if not Product.objects.filter(id=product_id).exists():
-                return JsonResponse({"MESSAGE": "NOT_FOUND"}, status=404)
 
-            product = Product.objects.get(id=product_id)
+        if not is_exists_product(product_id):
+            return JsonResponse({"MESSAGE": MESSAGE["404"]}, status=404)
 
-            result = {
-                "product_id"    : product.id,
-                "title"         : product.title,
-                "description"   : product.description,
-                "price"         : product.price,
-                "category_name" : product.category.name,
-                "menu_name"     : product.category.menu.name,
-                "discount_rate" : product.productdetail_set.first().discount_rate,
-                "capacity"      : product.productdetail_set.first().capacity,
-                "kcal"          : product.productdetail_set.first().kcal,
-                "content"       : product.productdetail_set.first().content,
-                "images"        : [
-                    product.image_url for product in product.productimage_set.all()
-                ],
-                "tags"          : [
-                    product.tag.name for product in product.producttag_set.all()
-                ],
-            }
+        product = Product.objects.get(id=product_id)
 
-            return JsonResponse({"RESULT": result}, status=200)
+        result = {
+            "product_id"    : product.id,
+            "title"         : product.title,
+            "description"   : product.description,
+            "price"         : product.price,
+            "category_name" : product.category.name,
+            "menu_name"     : product.category.menu.name,
+            "discount_rate" : product.productdetail_set.first().discount_rate,
+            "capacity"      : product.productdetail_set.first().capacity,
+            "content"       : product.productdetail_set.first().content,
+            "images"        : [
+                product.image_url for product in product.productimage_set.all()
+            ],
+            "tags"          : [
+                product.tag.name for product in product.producttag_set.all()
+            ],
+        }
 
-        except KeyError:
-            return JsonResponse({"MESSAGE": "KEY_ERROR"}, status=400)
-
+        return JsonResponse({"RESULT": result}, status=200)
 
 class ProductOptionView(View):
     def get(self, request, product_id):
-        try:
-            if not Product.objects.filter(id=product_id).exists():
-                return JsonResponse({"MESSAGE": "NOT_FOUND"}, status=404)
 
-            product       = Product.objects.get(id=product_id)
-            discount_rate = product.productdetail_set.all().first().discount_rate
+        if not is_exists_product(product_id):
+            return JsonResponse({"MESSAGE": MESSAGE["404"]}, status=404)
 
-            result = [
-                {
-                    "product_id"   : product_id,
-                    "option_id"    : option.id,
-                    "option_name"  : option.name,
-                    "option_price" : int(option.price * (1 - discount_rate)),
-                }
-                for option in product.productoption_set.all()
-            ]
+        product       = Product.objects.get(id=product_id)
+        discount_rate = product.productdetail_set.first().discount_rate
 
-            return JsonResponse({"RESULT": result}, status=200)
-        except KeyError:
-            return JsonResponse({"MESSAGE": "KEY_ERROR"}, status=400)
+        result = [
+            {
+                "product_id"   : product_id,
+                "option_id"    : option.id,
+                "option_name"  : option.name,
+                "option_price" : int(option.price * (1 - discount_rate)),
+            }
+            for option in product.productoption_set.all()
+        ]
+
+        return JsonResponse({"RESULT": result}, status=200)
+
+class ProductReviewView(View):
+    def get(self, request, product_id):
+
+        if not is_exists_product(product_id):
+            return JsonResponse({"MESSAGE": MESSAGE["404"]}, status=404)
+
+        result = [
+            {
+            }
+        ]
+
+def is_exists_product(product_id):
+    return Product.objects.filter(id=product_id).exists()
